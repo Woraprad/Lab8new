d78af3c6d4a5b7c17a790b037c3324f1d136dac1
Woraprad <58030217@kmitl.ac.th>
2017-11-15 21:30:13 +0700

    การวาดส่วนโค้ง (Arc)

--- Lab8new/Lab8new/Form1.cs
+++ Lab8new/Lab8new/Form1.cs
@@ -21,14 +21,13 @@ namespace Lab8new
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Rectangle rect = new Rectangle(10, 10, 100, 100);
-            g.DrawEllipse(Pens.Purple, rect);
-            Rectangle rect1 = new Rectangle(10, 120, 100, 100);
-            g.FillEllipse(Brushes.DeepPink, rect1);
-            Rectangle rect2 = new Rectangle(150, 10, 120, 100);
-            g.DrawEllipse(Pens.DarkSlateBlue, rect2);
-            Rectangle rect3 = new Rectangle(10, 10, 100, 100);
-            g.FillEllipse(Brushes.Firebrick, rect3);
+            Pen penEllipse = new Pen(Color.Brown);
+            penEllipse.DashStyle = DashStyle.Dash;
+            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
+
+            Pen penArc = new Pen(Color.Magenta, 2);
+            g.DrawArc(penArc, 20, 20, 200, 150, 90, 180);
+
 
             g.Dispose();
         }
