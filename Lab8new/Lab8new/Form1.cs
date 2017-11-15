85b18bbe5cac4aef0ca8ea47c9e8bb0ac60f3b0c
Woraprad <58030217@kmitl.ac.th>
2017-11-15 21:01:07 +0700

    การวาดสี่เหลี่ยมครั้งละรูป

--- Lab8new/Lab8new/Form1.cs
+++ Lab8new/Lab8new/Form1.cs
@@ -21,24 +21,12 @@ namespace Lab8new
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            GraphicsPath gp = new GraphicsPath();
-            gp.AddCurve(new Point[] {
-                new Point(100,50),
-                new Point(105,40),
-                new Point(120,40),
-                new Point(130,65),
-                new Point(100,100)
-            }, 0.5f);
+            Pen myPen = new Pen(Color.Blue, 3);
+            g.DrawRectangle(myPen, 10, 120, 100, 100);
 
-            gp.AddCurve(new Point[] {
-                new Point(100,100),
-                new Point(70,65),
-                new Point(80,40),
-                new Point(95,40),
-                new Point(100,50)
-            }, 0.5f);
+            Rectangle rect = new Rectangle(10, 10, 100, 100);
 
-            g.DrawPath(new Pen(Color.Red, 3), gp);
+            g.DrawRectangle(myPen, rect);
             g.Dispose();
         }
     }
